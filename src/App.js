ac34b57d07b9a6dd2ca3aaec8afb765d8e9af570
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-24 08:19:46 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from "react";
+import convertExcelToJson from "./excelToJson";
 
 function App() {
+  const convertedExel = async () => {
+    try {
+      const jsonData = await convertExcelToJson("your_excel_file_url.xlsx");
+      console.log(jsonData);
+      // Now you can use jsonData in your React component
+    } catch (error) {
+      // Handle errors
+    }
+  };
+
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Exel To Json</h1>
