99e3889ef78b42547c50f6feef1bfb05d252b560
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-24 09:33:38 +0100

    update

--- src/App.js
+++ src/App.js
@@ -24,17 +24,27 @@ function App() {
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Exel To Json</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="file"
-          value={formData.input}
-          onChange={(e) => setFormData({ ...formData, input: e.target.value })}
-          id="input"
-          autoComplete="on"
-        />
-        <button>Convert</button>
+    <div className="w-full h-full flex items-center justify-center bg-slate-400">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="flex flex-col items-center justify-center">
+          <div className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="file"
+              value={formData.input}
+              onChange={(e) =>
+                setFormData({ ...formData, input: e.target.value })
+              }
+              id="input"
+              autoComplete="on"
+            />
+          </div>
+
+          <button className="bg-blue-400 p-2 px-4 m-4 rounded">Convert</button>
+        </div>
       </form>
     </div>
   );
